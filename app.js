fb4cf1e7508cd6484268684dec72d74913f543fa
RichardGnz <gonzarichard376@gmail.com>
2024-07-22 23:56:35 -0400

    Agregando app.js e index.html

---
+++ app.js
@@ -0,0 +1,33 @@
+
+let dia= 0; // 1= lunes, 2= martes....
+            // 0 = domingo
+
+
+switch (dia){
+
+    case 0:
+        console.log('Es domingo');
+        break;
+    case 1:
+        console.log('Es Lunes');
+        break;
+    case 2:
+        console.log('Es Martes');
+        break;
+    case 3:
+        console.log('Es Miercoles');
+        break;
+    case 4:
+        console.log('Es Jueves');
+       break;
+    case 5:
+        console.log('Es Viernes');
+        break;
+    case 5:
+        console.log('Es Sabado');
+        break;
+    
+    default:
+        console.log('El dia ingresado no es valido');
+
+}
